d88f0e67b6e0874b408301ff1ef084c2295916e6
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-18 05:14:50 +0200

    Add GooglesheetsVideos class for video processing

---
+++ src/sevices/googlesheets_videos.py
@@ -0,0 +1,29 @@
+class GooglesheetsVideos (VideoProcessing):
+  
+    def __init__(self, worksheet):
+      
+        self.worksheet = wroksheet
+
+
+    def get_videos(self) -> list[Video]:
+      
+        records = self.worksheet.get_all_records()
+      
+        videos = []
+      
+        for record in records:
+            if record['Download Status'] != 'Pending':
+                continue
+
+            video = Video(
+              id = str(record['ID']),
+              pinterest_link = record['Pinterest Links'],
+              download_status = record['Dowload Status'],
+              drobox_uploaded = record['Drobox Uploaded'],
+              remark = record['Remark'],
+            )
+
+            videos.append(video)
+
+        return videos
+              
