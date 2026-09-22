2b1b9e5acdf677eedf70dac99899709b7d0c6e6a
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-22 17:57:45 +0700

    Integrate Google Sheets client into main.py
    Import GooglesheetsClient and initialize it in main function.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print('Github action start running')
+from src.sevices.googlesheets_client import GooglesheetsClient
+
+
+def main():
+  
+    print('Github action start running')
+    google_client = GooglesheetsClient(service_account_json = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'], spreadsheet_id = os.enviro['GOOGLE_SPREADSHEET_ID'])
+    pinterest_sheet = google_client.get_worksheet('pinterest')
+
+
+if __name__ == '__main__':
+    main()
