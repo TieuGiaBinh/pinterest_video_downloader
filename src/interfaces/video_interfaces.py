886421b73f472b626e489cd672fd2aa71af9b045
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-23 05:34:19 +0200

    Rename VideoProcessing class to VideoInterfaces

--- src/interfaces/video_interfaces.py
+++ src/interfaces/video_interfaces.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from src.models.video import Video
 
 
-class VideoProcessing (ABC):
+class VideoInterfaces (ABC):
     @abstractmethod
     def get_pending_videos(self) -> list[Video]:
         pass
