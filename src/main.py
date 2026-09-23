5d8a0acc4ec8d5b713c59d6d8b16792b941b8050
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-23 05:40:57 +0200

    Replace GoogleSheetsVideos with PinterestInfo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.sevices.googlesheets_client import GoogleSheetsClient
-from src.sevices.googlesheets_videos import GoogleSheetsVideos
+from src.sevices.pinterest_info import PinterestInfo
 import os
 
 
@@ -10,8 +10,8 @@ def main():
     google_client = GoogleSheetsClient(service_account_json = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'], spreadsheet_id = os.environ['GOOGLE_SPREADSHEET_ID'])
     pinterest_sheet = google_client.get_worksheet('pinterest')
 
-    google_sheet = GoogleSheetsVideo(pinterest_sheet)
-    pinterest_videos = google_sheet.get_videos()
+    pinterest_info = PinterestInfo(pinterest_sheet)
+    pinterest_videos = pinterest_info.get_videos()
 
     print(f'ID: {pinterest_videos[0].id}')
     print(f'Pinterest link: {pinterest_videos[0].pinterest_link}')
