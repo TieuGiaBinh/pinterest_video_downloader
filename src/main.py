10e9a147edbf55b2bb6cf0aa0f924bc9bf03edb6
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-23 05:17:25 +0200

    Fix import and class name typos in main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 from src.sevices.googlesheets_client import GoogleSheetsClient
-from src.sevices.googlesheets_videos import GooglesheetsVideos
+from src.sevices.googlesheets_videos import GooglesSheetsVideos
 
 
 def main():
   
     print('Github action start running')
   
-    google_client = GooglesheetsClient(service_account_json = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'], spreadsheet_id = os.enviro['GOOGLE_SPREADSHEET_ID'])
+    google_client = GoogleSheetsClient(service_account_json = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'], spreadsheet_id = os.enviro['GOOGLE_SPREADSHEET_ID'])
     pinterest_sheet = google_client.get_worksheet('pinterest')
 
-    google_sheet = GooglesheetsVideo(pinterest_sheet)
+    google_sheet = GooglesSheetsVideo(pinterest_sheet)
     pinterest_videos = google_sheet.get_videos()
 
     print(f'ID: {pinterest_videos[0].id}')
