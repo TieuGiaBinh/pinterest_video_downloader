19d4ef5c9015849d0e5a2e4681cf3e99300f6be8
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-23 05:14:01 +0200

    Fix import statement for GoogleSheetsVideos

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.sevices.googlesheets_client import GoogleSheetsClient
-from src.sevices.googlesheets_videos import GoogleSheetsVideos
+from src.sevices.googlesheets_videos import GooglesheetsVideos
 
 
 def main():
