6e544b1b851e7698f07174adb1ff430e2a872b21
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-23 05:02:47 +0200

    Refactor Google Sheets client and add video functionality
    Renamed GooglesheetsClient to GoogleSheetsClient and added GoogleSheetsVideo import. Updated main function to utilize GoogleSheetsVideo for fetching videos.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
-from src.sevices.googlesheets_client import GooglesheetsClient
+from src.sevices.googlesheets_client import GoogleSheetsClient
+from src.sevices.googlesheets_video import GoogleSheetsVideo
 
 
 def main():
   
     print('Github action start running')
+  
     google_client = GooglesheetsClient(service_account_json = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'], spreadsheet_id = os.enviro['GOOGLE_SPREADSHEET_ID'])
     pinterest_sheet = google_client.get_worksheet('pinterest')
+
+    google_sheet = GoogleSheetsVideo(pinterest_sheet)
+    pinterest_videos = google_sheet.get_video()
+
+    print(pinterest_videos)
 
 
 if __name__ == '__main__':
