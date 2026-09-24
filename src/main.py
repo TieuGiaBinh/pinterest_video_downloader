68028879394260c4cbc9eb45a5e6067991a86c69
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-24 13:34:42 +0700

    Refactor Pinterest integration to use GoogleSheetInfo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.sevices.googlesheets_client import GoogleSheetsClient
-from src.sevices.pinterest_info import PinterestInfo
+from src.sevices.googlesheets_info import GoogleSheetInfo
 import os
 
 
@@ -8,10 +8,10 @@ def main():
     print('Github action start running')
   
     google_client = GoogleSheetsClient(service_account_json = os.environ['GOOGLE_SERVICE_ACCOUNT'], spreadsheet_id = os.environ['GOOGLE_SPREADSHEET_ID'])
-    pinterest_sheet = google_client.get_worksheet('pinterest')
+    work_sheet = google_client.get_worksheet('pinterest')
 
-    pinterest_info = PinterestInfo(pinterest_sheet)
-    pinterest_videos = pinterest_info.get_videos()
+    google_sheet = GoogleSheetInfo(work_sheet)
+    pinterest_videos = google_sheet.get_videos()
 
     print(f'ID: {pinterest_videos[0].id}')
     print(f'Pinterest link: {pinterest_videos[0].pinterest_link}')
