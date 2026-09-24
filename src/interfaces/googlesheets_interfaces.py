8b51a94cda355b3bfe92a2fb073cac88309c4155
TieuGiaBinh <73636576+TieuGiaBinh@users.noreply.github.com>
2026-09-24 13:29:40 +0700

    Rename VideoInterfaces to GoogleSheetsInterfaces

--- src/interfaces/googlesheets_interfaces.py
+++ src/interfaces/googlesheets_interfaces.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from src.models.video import Video
 
 
-class VideoInterfaces (ABC):
+class GoogleSheetsInterfaces (ABC):
     @abstractmethod
     def get_pending_videos(self) -> list[Video]:
         pass
